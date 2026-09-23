99d3e5ae53f7dd88278ca5af1c1f5a7cbe95e5c0
SachinK-DevOps <2022wc86025@wilp.bits-pilani.ac.in>
2026-09-24 01:35:11 +0530

    Modify application source for CI test

--- src/main/java/com/bits/app/App.java
+++ src/main/java/com/bits/app/App.java
@@ -2,6 +2,6 @@ package com.bits.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("BITS Jenkins Continuous Integration Lab");
+        System.out.println("BITS Jenkins Continuous Integration Lab - Updated");
     }
 }
